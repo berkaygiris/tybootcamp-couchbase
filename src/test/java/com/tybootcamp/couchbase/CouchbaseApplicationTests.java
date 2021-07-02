8fbe1578651d34585560b39e8c2eecac46cc1a7a
Bgiris <berkaygiris@gmail.com>
2021-07-02 19:13:44 +0300

    init project

---
+++ src/test/java/com/tybootcamp/couchbase/CouchbaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tybootcamp.couchbase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouchbaseApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
