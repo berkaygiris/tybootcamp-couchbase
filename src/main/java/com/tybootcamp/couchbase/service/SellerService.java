a5dc9b394a9ed8747999d383bda44e805ea94265
Halil Eren <hlbrmm@gmail.com>
2021-07-11 13:31:55 +0300

    Couchbase Hw - findByName, addProductsToSeller implemented on SellerService

--- src/main/java/com/tybootcamp/couchbase/service/SellerService.java
+++ src/main/java/com/tybootcamp/couchbase/service/SellerService.java
@@ -3,41 +3,52 @@ package com.tybootcamp.couchbase.service;
 import com.tybootcamp.couchbase.domain.Product;
 import com.tybootcamp.couchbase.domain.Seller;
 import com.tybootcamp.couchbase.repository.SellerRepository;
+
 import java.util.List;
+import java.util.Locale;
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class SellerService {
 
-  private final SellerRepository sellerRepository;
+    private final SellerRepository sellerRepository;
 
-  public SellerService(SellerRepository sellerRepository) {
-    this.sellerRepository = sellerRepository;
-  }
+    public SellerService(SellerRepository sellerRepository) {
+        this.sellerRepository = sellerRepository;
+    }
 
-  public Seller create(String name) {
-    Seller seller = new Seller(name);
-    return sellerRepository.save(seller);
-  }
+    public Seller create(String name) {
 
-  public Seller findById(String id) {
-    return sellerRepository.findById(id).orElseThrow(() -> new RuntimeException(
-        String.format("Seller not found with id: %s", id))
-    );
-  }
+        if (!sellerRepository.existsByName(name)) {
+            Seller seller = new Seller(name);
+            return sellerRepository.save(seller);
+        } else
+            throw new RuntimeException(String.format("There is already a shop named as: %s", name));
+    }
 
-  public Seller findByName(String name) {
-    //TODO: Not yet implemented
-    throw new RuntimeException("Implement me");
-  }
+    public Seller findById(String id) {
+        return sellerRepository.findById(id).orElseThrow(() -> new RuntimeException(
+                String.format("Seller not found with id: %s", id))
+        );
+    }
 
-  public void addProductsToSeller(String sellerName, List<Product> products) {
-    //TODO: Not yet implemented
-    throw new RuntimeException("Implement me");
-  }
+    public Seller findByName(String name) {
+        Optional<Seller> optionalSeller = sellerRepository.findByName(name);
+        return optionalSeller.orElseThrow(() -> new RuntimeException(
+                String.format("Seller not found with name: %s", name))
+        );
+    }
 
-  public List<Product> getProductsByCategory(String sellerName, String category) {
-    //TODO: Not yet implemented
-    throw new RuntimeException("Implement me");
-  }
+    public void addProductsToSeller(String sellerName, List<Product> products) {
+        Seller seller = this.findByName(sellerName);
+        seller.setProducts(products);
+        sellerRepository.save(seller);
+    }
+
+    public List<Product> getProductsByCategory(String sellerName, String category) {
+        //TODO: Not yet implemented
+        throw new RuntimeException("Implement me");
+    }
 }
